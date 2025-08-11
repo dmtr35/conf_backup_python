0e8dce030bcedb9b0744033eaf23dcef522c1fdc
dmtr35 <nuke-id@yandex.ru>
2025-08-11 16:29:38 +0300

    Auto-commit: 11-08-2025 16:29:38 Added:    .gitignore Modified: setting/check_permission.py

--- setting/check_permission.py
+++ setting/check_permission.py
@@ -5,6 +5,6 @@ import sys
 def check_permission():
     if os.geteuid() != 0:
         print("This script must be run as root!")
-        sys.exit(1)                                 # Exit with error
+        # sys.exit(1)                                 # Exit with error
     else:
         print("Running as root.")
